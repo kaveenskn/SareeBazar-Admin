e974bc8c6d03f1dc0a711f78eb2897887142093d
kaveenskn <shanmugarajakaveen4@gmail.com>
2026-05-20 21:50:16 +0530

    initialized backend with express

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
