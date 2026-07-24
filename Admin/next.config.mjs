531a99a00ec987ecd94a559a8da7c3775eeb2a4d
AjayAR <ajayperies10@gmail.com>
2026-07-24 15:37:58 +0530

    feat: add Product Mongoose model and configure API rewrites in next.config.js

--- Admin/next.config.mjs
+++ Admin/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/backend/:path*",
-        destination: "http://127.0.0.1:5001/api/:path*",
+        destination: "http://127.0.0.1:5000/api/:path*",
       },
     ];
   },
